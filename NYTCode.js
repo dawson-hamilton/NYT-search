6be8ced67eaa63990c78af1ad66cebac64cc8f41
PRISON.MIKE <dawsondhamilton@gmail.com>
2019-12-19 20:44:21 -0700

    adding the url for the ajax call

--- NYTCode.js
+++ NYTCode.js
@@ -1,15 +1,30 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key={{NYTkey}}"
+function buildQueryUrl() {
+    var searchTerm = $("#search-term").val().trim();
+    console.log(searchTerm);
+    var startYear = $("#start-year").val().trim();
+    console.log(startYear);
+    var endYear = $("#end-year").val().trim();
+    console.log(endYear);
+    var apiKey = "rKwNj5dE97ykNAlHp4AO4vsbEFhuAHRc"
+    var nytimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    var url = nytimes + "q=" + searchTerm + "&begin_date=" + startYear + "&end_date" + endYear + "&api-key=" + apiKey;
+    console.log(url);
+    return url;
+}
 
-$("#run-search").on("click", function () {
-    $.ajax({ URL: queryURL, method: "GET" })
+function clear() {
+    $("#article-section").empty();
+}
+
+$("#run-search").on("click", function (event) {
+    event.preventDefault();
+
+    clear();
+    var url = buildQueryUrl();
+    console.log(url);
+
+    $.ajax({ URL: url, method: "GET" })
         .then(function (data) {
             console.log(data.response);
         });
-    var searchTerm = $("#search-term").val().trim();
-    var startYear = $("#start-year").val().trim();
-    var endYear = $("#end-year").val().trim();
-    console.log(searchTerm);
-    console.log(startYear);
-    console.log(endYear);
 });
-
