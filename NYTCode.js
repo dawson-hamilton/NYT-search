fd591709ca00ea507c2a822f06bde032831f44d7
PRISON.MIKE <dawsondhamilton@gmail.com>
2019-12-19 19:40:28 -0700

    grabbing the inupt of three of the boxes

--- NYTCode.js
+++ NYTCode.js
@@ -1,6 +1,16 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + articleSearch + "&api-key=rKwNj5dE97ykNAlHp4AO4vsbEFhuAHRc"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key={{NYTkey}}"
 
 $.ajax({ URL: queryURL, method: "GET" })
-    .then(function (response) {
-
+    .then(function (data) {
+        console.log(data.response);
     });
+
+$("#run-search").on("click", function () {
+    var searchTerm = $("#search-term").val().trim();
+    var startYear = $("#start-year").val().trim();
+    var endYear = $("#end-year").val().trim();
+    console.log(searchTerm);
+    console.log(startYear);
+    console.log(endYear);
+});
+
