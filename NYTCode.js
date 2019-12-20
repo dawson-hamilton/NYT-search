fe71bd1419ffd0e083c0faae0fb0e32d031d217d
PRISON.MIKE <dawsondhamilton@gmail.com>
2019-12-19 19:49:44 -0700

    moving ajax call

--- NYTCode.js
+++ NYTCode.js
@@ -1,11 +1,10 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key={{NYTkey}}"
 
-$.ajax({ URL: queryURL, method: "GET" })
-    .then(function (data) {
-        console.log(data.response);
-    });
-
 $("#run-search").on("click", function () {
+    $.ajax({ URL: queryURL, method: "GET" })
+        .then(function (data) {
+            console.log(data.response);
+        });
     var searchTerm = $("#search-term").val().trim();
     var startYear = $("#start-year").val().trim();
     var endYear = $("#end-year").val().trim();
